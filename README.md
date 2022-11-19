070384009b1ccb7651387160a67b74845a687896
VenPoisen <alanmf.pro@gmail.com>
2022-11-19 17:11:20 +0100

    Models creation
    In this update I created two models (contact, category) in to the database. These models will be where contact data will be stored.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Django-Phonebook-Project
- In this project i used Django to create a contacts agenda with name, phone, email that is located in an SQL database and can be access by the users logged in
+ In this project I used Django to create a contacts agenda with name, phone, email that is located in an SQL database and can be access by the users logged in
--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=255, blank=True)
+    telephone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    criation_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
