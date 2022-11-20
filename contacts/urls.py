2b02712de0d669e234984b85320f416e5885012d
VenPoisen <alanmf.pro@gmail.com>
2022-11-20 21:45:28 +0100

    Search field
    Search field on the main page, which can search contacts by first name, last name or phone number

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:contact_id>', views.open_contact, name='open_contact'),
+    path('search/>', views.search, name='search'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Contact
 from django.http import Http404
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models. functions import Concat
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.order_by('-id')
     paginator = Paginator(contacts, 10)
 
     page = request.GET.get('p')
@@ -19,6 +21,36 @@ def index(request):
 def open_contact(request, contact_id):
     # contact = Contact.objects.get(id=contact_id)
     contact = get_object_or_404(Contact, id=contact_id)
+
+    if not contact.show:
+        raise Http404()
+
     return render(request, 'contacts/open_contact.html', {
         'contact': contact
+    })
+
+
+def search(request):
+    term = request.GET.get('term')
+
+    if term is None:
+        raise Http404()
+
+    fields = Concat('name', Value(' '), 'last_name')
+
+    # contacts = Contact.objects.order_by(
+    #     '-id').filter(Q(name__icontains=term) | Q(last_name__icontains=term),)
+    contacts = Contact.objects.annotate(
+        full_name=fields
+    ).filter(
+        Q(full_name__icontains=term) | Q(
+            telephone__icontains=term)
+    )
+    paginator = Paginator(contacts, 10)
+
+    page = request.GET.get('p')
+    contacts = paginator.get_page(page)
+
+    return render(request, 'contacts/search.html', {
+        'contacts': contacts
     })
