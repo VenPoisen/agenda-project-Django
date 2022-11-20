15c6f62c010706dfde454535938dba4ddf1e7fb8
VenPoisen <alanmf.pro@gmail.com>
2022-11-20 20:34:57 +0100

    Conditionals
    Show - not show contacts in agenda

--- contacts/admin.py
+++ contacts/admin.py
@@ -4,11 +4,12 @@ from .models import Category, Contact
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id',  'name', 'last_name',
-                    'telephone', 'email', 'category')
+                    'telephone', 'email', 'category', 'show')
     list_display_links = ('id',  'name', 'last_name')
     list_filter = ('category',)
     list_per_page = 10
     search_fields = ('id', 'name', 'last_name', 'telephone', 'email')
+    list_editable = ('telephone', 'show')
 
 
 admin.site.register(Category)
