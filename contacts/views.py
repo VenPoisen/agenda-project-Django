5abfded6609fd087a188c91dbdef98a599c20f1e
VenPoisen <alanmf.pro@gmail.com>
2022-11-20 20:52:31 +0100

    Pagination
    Creating a paginator on the main page using bootstrap class

--- contacts/views.py
+++ contacts/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contact
 from django.http import Http404
+from django.core.paginator import Paginator
 
 
 def index(request):
     contacts = Contact.objects.all()
+    paginator = Paginator(contacts, 10)
+
+    page = request.GET.get('p')
+    contacts = paginator.get_page(page)
+
     return render(request, 'contacts/index.html', {
         'contacts': contacts
     })
