9b0f38709a86794fdf781a0beb86ecc0fc183044
VenPoisen <alanmf.pro@gmail.com>
2022-11-22 20:25:46 +0100

    Setting the register page
    Setting up the form for a new user to register and be able to login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.validators import validate_email
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -11,7 +13,57 @@ def logout(request):
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method != 'POST':
+        return render(request, 'accounts/register.html')
+
+    name = request.POST.get('name')
+    last_name = request.POST.get('last_name')
+    email = request.POST.get('email')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    password2 = request.POST.get('password2')
+
+    if not name or not last_name or not email or not username or not password or not password2:
+        messages.error(request, 'You need to fulfill all the fields')
+        return render(request, 'accounts/register.html')
+
+    try:
+        validate_email(email)
+    except:
+        messages.error(request, 'Invalid email')
+        return render(request, 'accounts/register.html')
+
+    if len(password) < 6:
+        messages.error(
+            request, 'Password needs to have more than six characters')
+        return render(request, 'accounts/register.html')
+
+    if len(username) < 6:
+        messages.error(
+            request, 'User needs to have more than six characters')
+        return render(request, 'accounts/register.html')
+
+    if password != password2:
+        messages.error(
+            request, 'Passwords are not the same')
+        return render(request, 'accounts/register.html')
+
+    if User.objects.filter(username=username).exists():
+        messages.error(
+            request, 'Username already exists')
+        return render(request, 'accounts/register.html')
+
+    if User.objects.filter(email=email).exists():
+        messages.error(
+            request, 'Email already exists')
+        return render(request, 'accounts/register.html')
+
+    messages.success(request, 'Successfully registered!')
+
+    user = User.objects.create_user(
+        username=username, email=email, password=password, first_name=name, last_name=last_name)
+    user.save()
+    return redirect('login')
 
 
 def dashboard(request):
