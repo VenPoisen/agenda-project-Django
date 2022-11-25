eb1be30f6cafa78d63d93bb960af79996c6f6aa1
VenPoisen <alanmf.pro@gmail.com>
2022-11-25 23:53:15 +0100

    db update

--- agenda/settings.py
+++ agenda/settings.py
@@ -80,11 +80,18 @@ WSGI_APPLICATION = 'agenda.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/agenda',
-        conn_max_age=60
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='postgresql://postgres:postgres@localhost:5432/agenda',
+#         conn_max_age=60
+#     )
+# }
 
 
 # Password validation
