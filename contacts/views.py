5e5542ebb5501d1c629757ab03ed6b7740cc8b6b
VenPoisen <alanmf.pro@gmail.com>
2022-11-26 00:33:34 +0100

    paginator update

--- contacts/views.py
+++ contacts/views.py
@@ -9,10 +9,10 @@ from django.contrib import messages
 
 def index(request):
     contacts = Contact.objects.order_by('-id')
-    paginator = Paginator(contacts, 20)
+    paginator = Paginator(contacts, 10)
 
     page = request.GET.get('p')
-    # contacts = paginator.get_page(page)
+    contacts = paginator.get_page(page)
 
     return render(request, 'contacts/index.html', {
         'contacts': contacts
